05e11faefbcd25b4b0d6f6ce632969f81469a9ac
BillyKuiper <456181@student.fontys.nl>
2021-12-08 19:47:02 +0100

    Created Base Files

---
+++ TestProject.WebshopAuth/AccountDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebshopAuth.Models;
+using WebshopAuth.Services;
+
+namespace TestProject.WebshopAuth
+{
+    public class AccountDB : IAccount
+    {
+        public User checkRegistered(User u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User getUserById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User login(User u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string loginNoToken(int id, int role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool register(User u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

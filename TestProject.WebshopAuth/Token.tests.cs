032b08dc8ebf4e0c0c03ed26a8c279395388ab61
BillyKuiper <456181@student.fontys.nl>
2021-12-20 22:11:01 +0100

    Added Tests

--- TestProject.WebshopAuth/Token.tests.cs
+++ TestProject.WebshopAuth/Token.tests.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebshopAuth.Controllers;
 
 namespace TestProject.WebshopAuth
 {
@@ -15,6 +17,45 @@ namespace TestProject.WebshopAuth
             
         }
 
+        [Test]
+        public void CreateToken()
+        {
+            //Arrange
+            var controller = new TokenController();
+
+            //Act
+            dynamic result = controller.CreateToken(1,0);
+            //Assert
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public void CreateTokenNoId()
+        {
+            //Arrange
+            var controller = new TokenController();
+
+            //Act
+            dynamic result = controller.CreateToken(0, 0);
+            int expectedResult = 400;
+
+            //Assert
+            Assert.AreEqual(result.StatusCode, expectedResult);
+        }
+
+        [Test]
+        public void CreateTokenNoRole()
+        {
+            //Arrange
+            var controller = new TokenController();
+
+            //Act
+            dynamic result = controller.CreateToken(0, 0);
+            int expectedResult = 400;
+
+            //Assert
+            Assert.AreEqual(result.StatusCode, expectedResult);
+        }
 
     }
 }
